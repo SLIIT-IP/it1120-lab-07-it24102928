cecc59288e2b67017f67f2c0e6f7e2ea2370d98f
it24102928 <it24102928@my.sliit.lk>
2024-09-30 21:36:33 +0530

    IT24102928  Madushani G.R

---
+++ Q2/IT24102928Lab7Q2A.java
@@ -0,0 +1,11 @@
+public class IT24102928Lab7Q2A {
+    public static void main(String[] args) {
+        char symb ='$';
+        for (int j =1;j<5;j++) {
+            for (int i = 1; i < 6; i++) {
+                System.out.print(symb);
+            }
+            System.out.println("\n");
+        }
+    }
+}
---
+++ Q2/IT24102928Lab7Q2B.java
@@ -0,0 +1,9 @@
+public class IT24102928Lab7Q2B {
+    public static void main(String[] args) {
+        String symb ="*";
+        for(int i = 1;i<6;i++){
+            System.out.println(i+" - "+symb);
+            symb = symb+"*";
+        }
+    }
+}
